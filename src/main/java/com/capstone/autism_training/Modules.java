7256f93ac77b05e88114171ee8ba43f1f3aeffb4
aadityarock2000 <aadityaparthasarathy@gmail.com>
2022-03-26 18:11:57 +0530

    Added Card Layout

--- src/main/java/com/capstone/autism_training/Modules.java
+++ src/main/java/com/capstone/autism_training/Modules.java
@@ -2,7 +2,9 @@ package com.capstone.autism_training;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Modules extends AppCompatActivity {
 
@@ -10,6 +12,10 @@ public class Modules extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modules);
+    }
+
+    public void showDecks_onClick(View view) {
+        startActivity(new Intent(this, DeckActivity.class));
     }
 }
 
